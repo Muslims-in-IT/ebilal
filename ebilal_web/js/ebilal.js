7d13af45e2d5d67e9996793b4ea5f24d926bf6a0
Yusuf Kaka <yusuf.kaka@mtn.com>
2021-12-11 17:39:14 +0200

    Added bulma for prettyness

--- ebilal_web/js/ebilal.js
+++ ebilal_web/js/ebilal.js
@@ -1,9 +1,17 @@
 /* main app that calls ebilal api */
-const url = "http://ebilal.local:8000/mounts"; 
-fetch(url) 
+const baseurl = "http://ebilal.local:8000/"; 
+fetch(baseurl+"mounts") 
   .then(response => response.text())  
-.then(html => {
-  // console.log(html);
-  document.getElementById('mounts').innerHTML = html;
+.then(response => {
+  const obj = JSON.parse(response);
+  document.getElementById('mounts').value = obj.mounts;
+})
+.catch((err) => console.log("Can’t access " + url + " response. Blocked by browser?" + err));
+
+fetch(baseurl+"volume") 
+  .then(response => response.text())  
+.then(response => {
+  const obj = JSON.parse(response);
+  document.getElementById('volume').value = obj.volume;
 })
 .catch((err) => console.log("Can’t access " + url + " response. Blocked by browser?" + err));
