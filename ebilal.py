f76d2fdef8841125745b7f3b54fae96c547ca5bc
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-01-25 17:30:36 +0200

    Added a play api

--- ebilal.py
+++ ebilal.py
@@ -32,80 +32,6 @@ journald_handler.setFormatter(logging.Formatter(
 # add the journald handler to the current logger
 logger.addHandler(journald_handler)
 
-#API stuff
-app = FastAPI(root_path="/api")
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-settings = LazySettings(settings_file="/opt/ebilal/settings.toml")
-
-if settings.default.audio_device == "":
-    try:
-        mixer = alsaaudio.Mixer()
-    except:
-        mixer = alsaaudio.Mixer(alsaaudio.mixers()[0])
-else:
-    mixer = alsaaudio.Mixer(settings.default.audio_device)
-current_vol = mixer.getvolume()[0]
-
-@app.get("/mounts")
-def read_mounts():
-    return {"mounts": settings.default.mounts}
-
-
-@app.post("/mounts/{mount}")
-def set_mount(mount: str):
-    settings.default.mounts = [mount]
-    write('settings.toml', settings.to_dict(), merge=False)
-    return {"mounts": settings.default.mounts}
-
-@app.post("/mounts")
-def write_mounts(mounts: List[str]):
-    settings.default.mounts = mounts
-    write('settings.toml', settings.to_dict() , merge=False)
-    return {"mounts": settings.default.mounts}
-
-@app.get("/server_url")
-def read_mounts():
-    return {"server_url": settings.default.server_url}
-
-@app.post("/server_url/")
-def write_url(url: str):
-    settings.default.server_url = url
-    write('settings.toml', settings.to_dict(), merge=True)
-    return {"mounts": settings.default.server_url}
-
-@app.get("/volume")
-def read_root():
-    return {"volume": mixer.getvolume()[0]}
-
-@app.post("/volume")
-def volset(vol:int):
-    mixer.setvolume(vol)
-    return {"volume": mixer.getvolume()}
-
-@app.post("/volume/up")
-def volup():
-    if (mixer.getvolume()[0] <= 90):
-        current_vol = mixer.getvolume()[0] + 10
-        mixer.setvolume(current_vol)
-    return {"volume": mixer.getvolume()}
-
-@app.post("/volume/down")
-def volup():
-    if (mixer.getvolume()[0] >= 10):
-        current_vol = mixer.getvolume()[0] - 10
-        mixer.setvolume(current_vol)
-    return {"volume": mixer.getvolume()}
-
 # LiveMasjid client
 class LivemasjidClient:
     """Livemasjid client Object"""
@@ -213,10 +139,90 @@ class LivemasjidClient:
     def getlivestreams(self):
         return self.livestreams
 
+livemasjid = LivemasjidClient()
+
+
+#API stuff
+app = FastAPI(root_path="/api")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+settings = LazySettings(settings_file="/opt/ebilal/settings.toml")
+
+if settings.default.audio_device == "":
+    try:
+        mixer = alsaaudio.Mixer()
+    except:
+        mixer = alsaaudio.Mixer(alsaaudio.mixers()[0])
+else:
+    mixer = alsaaudio.Mixer(settings.default.audio_device)
+current_vol = mixer.getvolume()[0]
+
+@app.get("/mounts")
+def read_mounts():
+    return {"mounts": settings.default.mounts}
+
+@app.post("/mounts/{mount}")
+def set_mount(mount: str):
+    settings.default.mounts = [mount]
+    write('settings.toml', settings.to_dict(), merge=False)
+    return {"mounts": settings.default.mounts}
+
+@app.post("/mounts")
+def write_mounts(mounts: List[str]):
+    settings.default.mounts = mounts
+    write('settings.toml', settings.to_dict() , merge=False)
+    return {"mounts": settings.default.mounts}
+
+@app.get("/server_url")
+def read_mounts():
+    return {"server_url": settings.default.server_url}
+
+@app.post("/server_url/")
+def write_url(url: str):
+    settings.default.server_url = url
+    write('settings.toml', settings.to_dict(), merge=True)
+    return {"mounts": settings.default.server_url}
+
+@app.get("/volume")
+def read_root():
+    return {"volume": mixer.getvolume()[0]}
+
+@app.post("/volume")
+def volset(vol:int):
+    mixer.setvolume(vol)
+    return {"volume": mixer.getvolume()}
+
+@app.post("/volume/up")
+def volup():
+    if (mixer.getvolume()[0] <= 90):
+        current_vol = mixer.getvolume()[0] + 10
+        mixer.setvolume(current_vol)
+    return {"volume": mixer.getvolume()}
+
+@app.post("/volume/down")
+def volup():
+    if (mixer.getvolume()[0] >= 10):
+        current_vol = mixer.getvolume()[0] - 10
+        mixer.setvolume(current_vol)
+    return {"volume": mixer.getvolume()}
+
+@app.get("/player/play")
+def play(mount:str):
+    livemasjid.playmount(mount)
+    return {"status": "playing"}
+
 def main():
     logger.info("Starting..")
     parser = argparse.ArgumentParser(description='Linux client for Livemasjid.com streams.')
-    livemasjid = LivemasjidClient()
     livemasjid.connect()
 
     #Setup the Pimoroni module if present
