83cadc42520645ada09ee80e7265208115d9fc60
Yusuf Kaka <yusufk@gmail.com>
2024-03-09 10:15:22 +0200

    More fixes to setup script

--- setup.sh
+++ setup.sh
@@ -2,12 +2,21 @@
 # Exit if any subcommand fails
 set -e
 printf "[1/5]⏳   Installing apt packages"
-sudo apt update && sudo apt -y install git python3 python3-pip ffmpeg ssh-client build-essential libsystemd-dev libasound2-dev nginx
+sudo apt update
+sudo apt -y install git python3 python3-pip python3-venv ffmpeg ssh-client build-essential libsystemd-dev libasound2-dev nginx
 printf "[2/5]⏳   Fetching eBilal code"
 cd /opt/
-sudo git clone https://github.com/Muslims-in-IT/ebilal
+if [ ! -d "ebilal" ]; then
+  sudo git clone https://github.com/Muslims-in-IT/ebilal
+else
+  echo "Directory ebilal already exists, skipping clone operation."
+fi
 printf "[2.5/5]⏳   Creating ebilal user and setting permissions"
-sudo adduser --disabled-password --gecos "" ebilal
+if id "ebilal" &>/dev/null; then
+    echo "User ebilal already exists, skipping adduser operation."
+else
+    sudo adduser --disabled-password --gecos "" ebilal
+fi
 sudo adduser ebilal users
 sudo adduser ebilal audio
 sudo chown -R ebilal:users ebilal
@@ -15,7 +24,7 @@ printf "[3/5]⏳   Fetching Python dependencies"
 cd ebilal
 sudo -u ebilal python3 -m venv venv
 source venv/bin/activate
-sudo -u ebilal pip3 install -r requirements.txt
+sudo -u ebilal pip3 install --no-cache-dir -r requirements.txt
 cp settings_example.toml settings.toml
 printf "[4/5]⏳   Setting up eBilal as a service"
 sudo cp other/*.service /lib/systemd/system/
